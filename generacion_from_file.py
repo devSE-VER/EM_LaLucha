50a93a267ed93828c3f9029e5e6c13b2e33d61e7
kevin-seguroenergy <klopez@seguroenergyllc.com>
2025-04-23 17:12:41 -0600

    EM La Lucha - Added validations of existing data and server connections.

--- generacion_from_file.py
+++ generacion_from_file.py
@@ -3,88 +3,96 @@
 import pandas as pd
 import re
 from openpyxl import load_workbook
-from sqlalchemy import create_engine
-from os import scandir
 from shutil import move
-from urllib.parse import quote_plus
 from constants import DIR_GEN, SERVERS, DRV
+from process.functions import get_files
+from process.database import check_data_exist, get_engines
 
-
-def get_files(path_gen, regex_str):
-    return [obj.name for obj in scandir(path_gen)
-            if obj.is_file() and re.search(regex_str, obj.name) and obj.name.endswith('.xlsx')]
-
-
-def imp_generation(srv, df_data):
-    con_data = srv
-    conn_str = f"mssql+pyodbc://{con_data['USER']}:{quote_plus(con_data['PW'])}@{con_data['SERVER']}/{con_data['DB']}?driver={DRV}"
-    engine = create_engine(conn_str, echo=False)
-
-    df_data.to_sql(name='Generacion', con=engine, if_exists='append', chunksize=1000,
-                   schema='dbo', index=False)
 
 
 def main():
     print(DIR_GEN)
+    # add DRV to servers data
+    for srv, srv_data in SERVERS.items():
+        srv_data['DRV'] = DRV
+    
     # LOAD PROFILE MAYO_LA LUCHA 2023_06062023.xlsx
-    pattern_file = '^LOAD PROFILE LA LUCHA(.*)'
+    pattern_file = '^LOAD PROFILE LA LUCHA(.*).xlsx'
     file_list = get_files(DIR_GEN, pattern_file)
-    if file_list:
-        print(file_list)
-        # Process file
-        for file in file_list:
-            # Get date
-            # The regex pattern that we created
-            pattern = '\d{8}'
-            # Will return all the strings that are matched
-            date_search = re.search(pattern, file)
-            print(date_search)
-            date_str = date_search.group()
-            opr_day = date_str[:2]
-            opr_month = date_str[2:4]
-            opr_year = date_str[4:8]
-            opr_dt = f'{opr_year}-{opr_month}-{opr_day}'
 
-            # TODO: Validate that doesn't exist data for opr_dt into the table
+    for file in file_list:
+        print(f"\n\n{file=}")
+        # Get date
+        # The regex pattern that we created
+        pattern = '(\d{2})(\d{2})(\d{4})'
+        opr_day, opr_month, opr_year = re.search(pattern, file).groups()
+        opr_dt = f'{opr_year}-{opr_month}-{opr_day}'
+        # print(f"{opr_dt=}")
 
-            print(date_str)
+        # Validate that doesn't exist data for opr_dt into each server's table
+        engines = get_engines(SERVERS)
+        for eng in engines:
+            try:
+                eng['file_exist'] = check_data_exist(eng['engine'], schema='dbo', table='Generacion', opr_dt=opr_dt)
+            except Exception as e:
+                print(f"Error trying to validate data on the {eng['srv']} server\n", e)
+                engines.remove(eng) # remove engine if the connection fails
 
-            month_str = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
-                         '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'NOV', '12': 'Dec'}
+        if all([eng['file_exist'] for eng in engines]):
+            print("Data alredy load on all servers!")
+            continue
 
-            path_file = f'{DIR_GEN}\\{file}'
-            path_loaded = f'{DIR_GEN}\\loaded\\{file}'
-            sheet_name = f"{opr_day}-{month_str[opr_month]}"
+        # file processing
+        month_str = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun',
+                        '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'NOV', '12': 'Dec'}
 
-            wb = load_workbook(path_file, data_only=True)
-            print(wb.sheetnames)
+        path_file = f'{DIR_GEN}\\{file}'
+        path_loaded = f'{DIR_GEN}\\loaded\\{file}'
+        sheet_name = f"{opr_day}-{month_str[opr_month]}"
 
-            ws = wb[sheet_name]
+        wb = load_workbook(path_file, data_only=True)
+        ws = wb[sheet_name]
 
-            gen_matrix = []
-            for i in range(3, 27):
-                opr_hr = ws[f"I{i}"].value
-                gen_mw = round(ws[f"K{i}"].value,2)
-                data = [opr_dt, opr_hr, gen_mw]
+        gen_matrix = []
+        for i in range(3, 27):
+            opr_hr = ws[f"I{i}"].value
+            gen_mw = round(ws[f"K{i}"].value,2)
+            data = [opr_dt, opr_hr, gen_mw]
 
-                gen_matrix.append(data)
+            gen_matrix.append(data)
 
-            print(gen_matrix)
-            cols = ['opr_dt', 'opr_hr', 'generacion_mw']
-            df_gen = pd.DataFrame(data=gen_matrix, columns=cols)
+        
+        cols = ['opr_dt', 'opr_hr', 'generacion_mw']
+        df_gen = pd.DataFrame(data=gen_matrix, columns=cols)
 
-            print(df_gen)
 
-            # Import to DB
-            if not df_gen.empty:
-                # Seguro SQL Server
-                imp_generation(SERVERS['SE'], df_gen)
+        if df_gen.empty:
+            print("Nothing to upload!")
+            continue
+        
+        # Import to DB
+        for eng in engines:
+            if not eng['file_exist']:
+                try:
+                    # print(f"({eng['srv']}) to sql...")
+                    df_gen.to_sql(name='Generacion', con=eng['engine'], if_exists='append', chunksize=1000, schema='dbo', index=False)
+                    print("UPLOADED!")
+                except Exception as e:
+                    print(f"Error trying to upload data on the {eng['srv']} server\n", e)
 
-                # Saavi SQL Server
-                imp_generation(SERVERS['SAAVI'], df_gen)
-
-                # Move file to loaded folder
-                move(path_file, path_loaded)
+        # If any connection failed previously
+        engines = get_engines(SERVERS)
+        for eng in engines:
+            eng['file_exist'] = False
+            try:
+                eng['file_exist'] = check_data_exist(eng['engine'], schema='dbo', table='Generacion', opr_dt=opr_dt)
+            except:
+                pass
+        
+        # Move file to loaded folder
+        if all([eng['file_exist'] for eng in engines]):
+            print("(Move)")
+            # move(path_file, path_loaded)
 
 
 if __name__ == '__main__':
