0e4a8e9f76bad8fea1e6d19beca34e282ed307c8
kevin-seguroenergy <klopez@seguroenergyllc.com>
2025-04-23 17:10:18 -0600

    EM La Lucha - Added check_data_exist and get_engines functions.

--- process/database.py
+++ process/database.py
@@ -18,9 +18,9 @@ def get_env_engine():
     return engine
 
 
+
 def check_file_exist(fname:str, cnx_str:str, schema:str, table:str):
-    engine = create_engine(cnx_str, echo=False)
-    
+    engine = create_engine(cnx_str, echo=False)    
     qry_str = text(f"select fname from {schema}.{table} where fname = '{fname}'")
 
     with engine.connect() as conn:
@@ -28,3 +28,34 @@ def check_file_exist(fname:str, cnx_str:str, schema:str, table:str):
         conn.close()
     
     return bool(file_exist)
+
+
+
+def check_data_exist(engine, schema:str, table:str, **kargs):
+    if not kargs:
+        raise ValueError('Missing query field!')
+
+    field, value = list(kargs.items())[0]
+    qry_str = f"select {field} from {schema}.{table} where {field} = '{value}';"
+
+    with engine.connect() as conn:
+        file_exist = conn.execute(text(qry_str)).scalar()
+        conn.close()
+
+    return bool(file_exist)
+
+
+
+def get_engines(servers:dict):
+    engines = []
+    for srv, cnx_data in servers.items():
+        conn_str = f"mssql+pyodbc://{cnx_data['USER']}:{quote_plus(cnx_data['PW'])}@{cnx_data['SERVER']}/{cnx_data['DB']}?driver={cnx_data['DRV']}"
+        engine = create_engine(conn_str, echo=False)
+        engines.append({
+            'srv':srv,
+            'conn_str': conn_str,
+            'engine':engine,
+            'db': cnx_data['DB'],
+            'file_exist':True
+        })
+    return engines
