507b2b3673cfff53c89a536816825a116b7f8629
kevin-seguroenergy <klopez@seguroenergyllc.com>
2025-04-25 21:33:30 -0600

    EM_LaLucha - Creating get_validated_engine function.

--- process/database.py
+++ process/database.py
@@ -59,3 +59,17 @@ def get_engines(servers:dict):
             'file_exist':True
         })
     return engines
+
+
+def get_validated_engine(cnx_data:str)->Engine:
+    conn_str = f"mssql+pyodbc://{cnx_data['USER']}:{quote_plus(cnx_data['PW'])}@{cnx_data['SERVER']}/{cnx_data['DB']}?driver={cnx_data['DRV']}"
+    try:
+        engine = create_engine(conn_str, echo=False)
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1")).scalar()
+        
+        return engine
+    
+    except Exception as e:
+        # print(f"Engine validation: {e}")
+        return None
